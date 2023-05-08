49d45e79055b052a26f1c3f475629b54ff965d00
kushal-kamra <kushalkamradev@gmail.com>
2023-05-08 12:05:43 +0530

    Added test case to update heading if user presses a button

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-test("Check Page Heading, Hello World", () => {
-    render(<App />);
-    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(/hello world/i);
+describe("App component", () => {
+    test("Check Page Heading, Hello World", () => {
+        render(<App />);
+        expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(/hello world/i);
+    });
+
+    test("Check Page Heading after user clicks button", () => {
+        render(<App />);
+        const button = screen.getByRole("button", { name: "Update Heading" });
+        userEvent.click(button);
+        expect(screen.getByRole("Hweading", { level: 1 })).toHaveTextContent(/hello project/i);
+    });
 });
